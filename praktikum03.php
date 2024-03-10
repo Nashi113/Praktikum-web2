b6a4bb894fd20dad279b5b1a59ea4aeeaba87dc8
Nashi113 <ikuameru12@gmail.com>
2024-03-10 23:07:26 +0700

    Menambahkan file

---
+++ praktikum03.php
@@ -0,0 +1,13 @@
+<?php
+// Ascending Loop
+echo "Looping Pertama\n";
+for ($i = 1; $i <= 20; $i++) {
+    echo "$i - I Love PHP \n" . "<br>";
+}
+
+// Descending Loop
+echo "\nLooping Kedua:\n";
+for ($i = 20; $i >= 1; $i--) {
+    echo "$i - I Love PHP \n" . "<br>";
+}
+?>
